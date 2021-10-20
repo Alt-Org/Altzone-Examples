f8f780448e80017832a00622a6073dd5fdc7edd8
Jari Petays <jari.petays@iki.fi>
2021-10-20 15:31:12 +0300

    synchronize ball color across network

--- Assets/Examples/Game/Scripts/Battle/Ball/BallColor.cs
+++ Assets/Examples/Game/Scripts/Battle/Ball/BallColor.cs
@@ -1,5 +1,6 @@
 using Examples.Config.Scripts;
 using Photon.Pun;
+using Prg.Scripts.Common.Photon;
 using Prg.Scripts.Common.PubSub;
 using UnityEngine;
 
@@ -7,14 +8,46 @@ namespace Examples.Game.Scripts.Battle.Ball
 {
     public class BallColor : MonoBehaviour
     {
+        private const int msgSetBallColor = PhotonEventDispatcher.eventCodeBase + 4;
+
+        private const int neutralColorIndex = 0;
+        private const int upperTeamColorIndex = 1;
+        private const int lowerTeamColorIndex = 2;
+
         [Header("Settings"), SerializeField] private SpriteRenderer _sprite;
         [SerializeField] private Color neutralColor;
         [SerializeField] private Color upperTeamColor;
         [SerializeField] private Color lowerTeamColor;
 
+        private PhotonEventDispatcher photonEventDispatcher;
+
         private void Awake()
         {
             Debug.Log("Awake");
+            photonEventDispatcher = PhotonEventDispatcher.Get();
+            photonEventDispatcher.registerEventListener(msgSetBallColor, data => { onSetBallColor(data.CustomData); });
+        }
+
+        private void sendSetBallColor(int colorIndex)
+        {
+            photonEventDispatcher.RaiseEvent(msgSetBallColor, colorIndex);
+        }
+
+        private void onSetBallColor(object data)
+        {
+            var colorIndex = (int)data;
+            switch (colorIndex)
+            {
+                case lowerTeamColorIndex:
+                    _sprite.color = lowerTeamColor;
+                    break;
+                case upperTeamColorIndex:
+                    _sprite.color = upperTeamColor;
+                    break;
+                default:
+                    _sprite.color = neutralColor;
+                    break;
+            }
         }
 
         private void OnEnable()
@@ -39,13 +72,13 @@ namespace Examples.Game.Scripts.Battle.Ball
             switch (data.newTeamIndex)
             {
                 case 0:
-                    _sprite.color = lowerTeamColor;
+                    sendSetBallColor(lowerTeamColorIndex);
                     break;
                 case 1:
-                    _sprite.color = upperTeamColor;
+                    sendSetBallColor(upperTeamColorIndex);
                     break;
                 default:
-                    _sprite.color = neutralColor;
+                    sendSetBallColor(neutralColorIndex);
                     break;
             }
         }
