2abcb0a8e6d628b1d7db5f867ed3729c28c080ca
Jari Petays <jari.petays@iki.fi>
2021-10-31 16:41:07 +0200

    move ball settings to separate BallSettings class and add some layer settings there

--- Assets/Examples2/Scripts/Ball.cs
+++ Assets/Examples2/Scripts/Ball.cs
@@ -1,23 +1,51 @@
-﻿using UnityEngine;
+﻿using Prg.Scripts.Common.Unity;
+using System;
+using UnityEngine;
 
 namespace Examples2.Scripts
 {
-    public class Ball : MonoBehaviour
+    [Serializable]
+    internal class BallSettings
     {
-        [SerializeField] private Vector2 initialVelocity;
-        [SerializeField] private float minVelocity = 3f;
+        [Header("Ball movement")] public Vector2 initialVelocity;
+        public float minVelocity;
+
+        [Header("Layers")] public LayerMask bounceMask;
+        public LayerMask teamAreaMask;
+        public LayerMask headMask;
+        public LayerMask brickMask;
+        public LayerMask wallMask;
+
+        [Header("Tags"), TagSelector] public string readTeamTag;
+        [TagSelector] public string blueTeamTag;
+    }
+
+    internal class Ball : MonoBehaviour
+    {
+        [SerializeField] private BallSettings settings;
 
         private Rigidbody2D _rigidbody;
         private Vector2 currentVelocity;
 
+        private int bounceMaskValue;
+        private int teamAreaMaskValue;
+        private int headMaskValue;
+        private int brickMaskValue;
+        private int wallMaskValue;
+
         private void Awake()
         {
+            bounceMaskValue = settings.bounceMask.value;
+            teamAreaMaskValue = settings.teamAreaMask.value;
+            headMaskValue = settings.headMask.value;
+            brickMaskValue = settings.brickMask.value;
+            wallMaskValue = settings.wallMask.value;
             _rigidbody = GetComponent<Rigidbody2D>();
         }
 
         private void OnEnable()
         {
-            _rigidbody.velocity = initialVelocity;
+            _rigidbody.velocity = settings.initialVelocity;
         }
 
         private void Update()
@@ -27,16 +55,23 @@ namespace Examples2.Scripts
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.gameObject.layer == 0)
+            var otherGameObject = other.gameObject;
+            var layer = otherGameObject.layer;
+            if (layer == 0)
             {
                 Debug.Log($"ignore {other.name} layer {other.gameObject.layer}");
                 return;
             }
-            Debug.Log($"hit {other.name} layer {other.gameObject.layer}");
-            var position = _rigidbody.position;
-            var closestPoint = other.ClosestPoint(position);
-            var direction = closestPoint - position;
-            bounceFrom(direction.normalized);
+            var colliderMask = 1 << layer;
+            if (bounceMaskValue == (bounceMaskValue | colliderMask))
+            {
+                var position = _rigidbody.position;
+                var closestPoint = other.ClosestPoint(position);
+                var direction = closestPoint - position;
+                bounceFrom(direction.normalized);
+                return;
+            }
+            Debug.Log($"UNHANDLED hit {other.name} layer {layer}");
         }
 
         private void bounceFrom(Vector2 collisionNormal)
@@ -44,7 +79,7 @@ namespace Examples2.Scripts
             var speed = currentVelocity.magnitude;
             var direction = Vector2.Reflect(currentVelocity.normalized, collisionNormal);
             Debug.Log($"bounce @ {_rigidbody.position} dir {direction}");
-            _rigidbody.velocity = direction * Mathf.Max(speed, minVelocity);
+            _rigidbody.velocity = direction * Mathf.Max(speed, settings.minVelocity);
         }
     }
 }
