df5e0345e0bf125b40bcc9a846d9201239b22f10
Jari Petays <jari.petays@iki.fi>
2021-11-02 11:40:28 +0200

    change ball color (locally) based on which team is active

--- Assets/Examples2/Scripts/Battle/Ball/BallController.cs
+++ Assets/Examples2/Scripts/Battle/Ball/BallController.cs
@@ -1,13 +1,24 @@
 using Examples2.Scripts.Battle.Factory;
 using Examples2.Scripts.Battle.interfaces;
 using Photon.Pun;
+using System;
 using System.Collections;
 using UnityEngine;
 
 namespace Examples2.Scripts.Battle.Ball
 {
+
     internal class BallController : MonoBehaviour
     {
+        [Serializable]
+        internal class State
+        {
+            public bool isRedTeamActive;
+            public bool isBlueTeamActive;
+        }
+
+        [SerializeField] private State state;
+
         public Vector2 ballVelocity;
         public Vector2 ballPosition;
         public bool startBallMoving;
@@ -83,14 +94,51 @@ namespace Examples2.Scripts.Battle.Ball
 
         private void onEnterTeamArea(GameObject other)
         {
-            Debug.Log($"onEnterTeamArea {other.tag}");
+            switch (other.tag)
+            {
+                case UnityConstants.Tags.RedTeam:
+                    state.isRedTeamActive = false;
+                    setBallColor(ball, state);
+                    return;
+                case UnityConstants.Tags.BlueTeam:
+                    state.isBlueTeamActive = false;
+                    setBallColor(ball, state);
+                    return;
+            }
+            Debug.Log($"UNHANDLED onEnterTeamArea {other.name} {other.tag}");
         }
 
         private void onExitTeamArea(GameObject other)
         {
-            Debug.Log($"onExitTeamArea {other.tag}");
+            switch (other.tag)
+            {
+                case UnityConstants.Tags.RedTeam:
+                    state.isRedTeamActive = true;
+                    setBallColor(ball, state);
+                    return;
+                case UnityConstants.Tags.BlueTeam:
+                    state.isBlueTeamActive = true;
+                    setBallColor(ball, state);
+                    return;
+            }
+            Debug.Log($"UNHANDLED onExitTeamArea {other.name} {other.tag}");
         }
 
         #endregion
+
+        private static void setBallColor(IBall ball, State state)
+        {
+            if (state.isRedTeamActive && !state.isBlueTeamActive)
+            {
+                ball.setColor(BallColor.RedTeam);
+                return;
+            }
+            if (state.isBlueTeamActive && !state.isRedTeamActive)
+            {
+                ball.setColor(BallColor.BlueTeam);
+                return;
+            }
+            ball.setColor(BallColor.NoTeam);
+        }
     }
 }
