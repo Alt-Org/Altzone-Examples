2ccf1f9a88679a591dc486ee7c5dee606af9844f
Jari Petays <jari.petays@iki.fi>
2021-12-02 08:34:49 +0200

    improve code

--- Assets/Altzone/Scripts/Window/DefaultWindow.cs
+++ Assets/Altzone/Scripts/Window/DefaultWindow.cs
@@ -1,26 +1,36 @@
-﻿using Altzone.Scripts.Window.ScriptableObjects;
+﻿using System.Diagnostics;
+using Altzone.Scripts.Window.ScriptableObjects;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Altzone.Scripts.Window
 {
     /// <summary>
-    /// Default window loader for any level that uses <c>WindowManager</c>.
+    /// Initial window loader for any level that uses <c>WindowManager</c>.
     /// </summary>
     public class DefaultWindow : MonoBehaviour
     {
         [SerializeField] private WindowDef _window;
-        [SerializeField] private GameObject _sceneWindow;
+        [SerializeField] private GameObject _sceneTestWindow;
 
         private void OnEnable()
         {
+            SetEditorStatus();
+
             Debug.Log($"OnEnable {_window}");
             var windowManager = WindowManager.Get();
             windowManager.SetWindowsParent(gameObject);
+            Assert.IsNotNull(_window.WindowPrefab, "_window.WindowPrefab != null");
+            windowManager.ShowWindow(_window);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        private void SetEditorStatus()
+        {
             if (_window.WindowPrefab == null)
             {
-                _window.SetWindowPrefab(_sceneWindow);
+                _window.SetWindowPrefab(_sceneTestWindow);
             }
-            windowManager.ShowWindow(_window);
         }
     }
 }
