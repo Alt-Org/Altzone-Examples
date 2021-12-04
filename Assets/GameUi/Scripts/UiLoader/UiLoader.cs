eee1f784930b92e8148cf518fa9f8e8f61194c8f
Jari Petays <jari.petays@iki.fi>
2021-12-04 15:35:59 +0200

    add more placeholder windows

---
+++ Assets/GameUi/Scripts/UiLoader/UiLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using Altzone.Scripts.Window;
+using Altzone.Scripts.Window.ScriptableObjects;
+using UnityEngine;
+
+namespace GameUi.Scripts.UiLoader
+{
+    public class UiLoader : MonoBehaviour
+    {
+        [SerializeField] private WindowDef _window;
+        private IEnumerator Start()
+        {
+            var wait = new WaitForSeconds(1f);
+            yield return wait;
+            WindowManager.Get().ShowWindow(_window);
+        }
+    }
+}
