8552137c0353eb6f2194fa39b28a9a5798f84031
Jari Petays <jari.petays@iki.fi>
2021-12-04 17:09:47 +0200

    add language selection

---
+++ Assets/GameUi/Scripts/LanguageSelection/LanguageSelectionController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GameUi.Scripts.LanguageSelection
+{
+    public class LanguageSelectionController : MonoBehaviour
+    {
+        [SerializeField] private LanguageSelectionView _view;
+
+        private void Awake()
+        {
+            // https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+            _view.LangButtonFi.onClick.AddListener(() => SetLanguage("fi"));
+            _view.LangButtonSe.onClick.AddListener(() => SetLanguage("sv"));
+            _view.LangButtonEn.onClick.AddListener(() => SetLanguage("en"));
+        }
+
+        private static void SetLanguage(string language)
+        {
+            Debug.Log($"SetLanguage {language}");
+        }
+    }
+}
---
+++ Assets/GameUi/Scripts/LanguageSelection/LanguageSelectionView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameUi.Scripts.LanguageSelection
+{
+    public class LanguageSelectionView : MonoBehaviour
+    {
+        [SerializeField] private Button _langButtonFi;
+        [SerializeField] private Button _langButtonSe;
+        [SerializeField] private Button _langButtonEn;
+
+        public Button LangButtonFi => _langButtonFi;
+        public Button LangButtonSe => _langButtonSe;
+        public Button LangButtonEn => _langButtonEn;
+    }
+}
