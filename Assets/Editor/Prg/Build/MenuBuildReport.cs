00ffaf94885e0141c211bd95a24cb362ceea5087
Jari Petays <jari.petays@iki.fi>
2022-01-03 10:46:03 +0200

    improve dev/build process support

--- Assets/Editor/Prg/Build/MenuBuildReport.cs
+++ Assets/Editor/Prg/Build/MenuBuildReport.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
@@ -11,10 +12,12 @@ namespace Editor.Prg.Build
 {
     public class MenuBuildReport : MonoBehaviour
     {
-        private static readonly string[] excludedFolders =
+        private static readonly string[] ExcludedFolders =
         {
             "Assets/Photon",
+            "Assets/Photon Unity Networking",
             "Assets/Editor",
+            "Assets/TextMesh Pro",
         };
 
         [MenuItem("Window/ALT-Zone/Build/Create Build Script", false, 1)]
@@ -32,8 +35,10 @@ namespace Editor.Prg.Build
         [MenuItem("Window/ALT-Zone/Build/Check Build Report", false, 9)]
         private static void WindowReport()
         {
+            var logWriter = new LogWriter();
+
             var buildTargetName = TeamCity.CommandLine.BuildTargetNameFrom(EditorUserBuildSettings.activeBuildTarget);
-            Debug.Log($"Build target is {buildTargetName}");
+            logWriter.Log($"Build target is {buildTargetName}");
             var buildReport = $"m_Build_{buildTargetName}.log";
             if (!File.Exists(buildReport))
             {
@@ -41,65 +46,78 @@ namespace Editor.Prg.Build
                 return;
             }
 
-            var allFiles = parseBuildReport(buildReport, out var totalSize);
-            Debug.Log($"Build contains {allFiles.Count} asset files, total size is {totalSize:### ### ###.0} kb");
+            var allFiles = ParseBuildReport(buildReport, out var totalSize);
+            logWriter.Log($"Build contains {allFiles.Count} total files, their size is {totalSize:### ### ##0.0} kb");
             var usedAssets = new HashSet<string>();
-            int[] fileCount = { 0, 0, 0 };
-            double[] fileSize = { 0, 0, 0 };
-            double[] filePercent = { 0, 0, 0 };
+            int[] fileCount = { 0, 0, 0, 0 };
+            double[] fileSize = { 0, 0, 0, 0 };
+            double[] filePercent = { 0, 0, 0, 0 };
             foreach (var assetLine in allFiles)
             {
-                if (assetLine.isAsset)
+                if (assetLine.IsAsset)
                 {
-                    usedAssets.Add(assetLine.filePath);
+                    usedAssets.Add(assetLine.FilePath);
                     fileCount[0] += 1;
-                    fileSize[0] += assetLine.fileSizeKb;
-                    filePercent[0] += assetLine.percentage;
+                    fileSize[0] += assetLine.FileSizeKb;
+                    filePercent[0] += assetLine.Percentage;
                 }
-                else if (assetLine.isPackage)
+                else if (assetLine.IsPackage)
                 {
                     fileCount[1] += 1;
-                    fileSize[1] += assetLine.fileSizeKb;
-                    filePercent[1] += assetLine.percentage;
+                    fileSize[1] += assetLine.FileSizeKb;
+                    filePercent[1] += assetLine.Percentage;
                 }
-                else if (assetLine.isResource)
+                else if (assetLine.IsResource)
                 {
                     fileCount[2] += 1;
-                    fileSize[2] += assetLine.fileSizeKb;
-                    filePercent[2] += assetLine.percentage;
+                    fileSize[2] += assetLine.FileSizeKb;
+                    filePercent[2] += assetLine.Percentage;
+                }
+                else if (assetLine.IsBuiltIn)
+                {
+                    fileCount[3] += 1;
+                    fileSize[3] += assetLine.FileSizeKb;
+                    filePercent[3] += assetLine.Percentage;
                 }
                 else
                 {
-                    Debug.LogError("Unknown asset line: " + assetLine.filePath);
+                    Debug.LogError("Unknown asset line: " + assetLine.FilePath);
                     return;
                 }
             }
-            Debug.Log($"Build contains {fileCount[0]} ASSET files, their size is {fileSize[0]:### ### ###.0} kb ({filePercent[0]:0.0}%)");
-            Debug.Log($"Build contains {fileCount[1]} PACKAGE files, their size is {fileSize[1]:### ### ###.0} kb ({filePercent[1]:0.0}%)");
-            Debug.Log($"Build contains {fileCount[2]} RESOURCE files, their size is {fileSize[2]:### ### ###.0} kb ({filePercent[2]:0.0}%)");
-            var unusedAssets = checkUnusedAssets(usedAssets);
-            Debug.Log($"Project contains {unusedAssets.Count} unused assets for {buildTargetName} build");
-            if (excludedAssetCount > 0)
+            logWriter.Log($"Build contains {fileCount[0]} ASSET files, their size is {fileSize[0]:### ### ##0.0} kb ({filePercent[0]:0.0}%)");
+            logWriter.Log($"Build contains {fileCount[1]} PACKAGE files, their size is {fileSize[1]:### ### ##0.0} kb ({filePercent[1]:0.0}%)");
+            logWriter.Log($"Build contains {fileCount[2]} RESOURCE files, their size is {fileSize[2]:### ### ##0.0} kb ({filePercent[2]:0.0}%)");
+            if (fileCount[3] > 0)
             {
-                Debug.Log($"Excluded {excludedAssetCount} files or folders");
+                logWriter.Log($"Build contains {fileCount[3]} Built-in files, their size is {fileSize[3]:### ### ##0.0} kb ({filePercent[3]:0.0}%)");
             }
-            double unusedAssetSizeTotal = unusedAssets.Select(x => x.fileSizeKb).Sum();
-            Debug.Log($"Unused assets total size is {unusedAssetSizeTotal:### ### ###.0} kb");
-            foreach (var unusedAsset in unusedAssets.OrderBy(x => x.fileSizeKb).Reverse())
+            var unusedAssets = CheckUnusedAssets(usedAssets, logWriter);
+            logWriter.Log($"Project contains {unusedAssets.Count} unused assets for {buildTargetName} build");
+            if (_excludedAssetCount > 0)
             {
-                Debug.Log($"UNUSED {unusedAsset.filePath} {unusedAsset.fileSizeKb} kb");
+                logWriter.Log($"Excluded {_excludedAssetCount} files or folders");
             }
+            double unusedAssetSizeTotal = unusedAssets.Select(x => x.FileSizeKb).Sum();
+            logWriter.Log($"Unused assets total size is {unusedAssetSizeTotal:### ### ##0.0} kb");
+            foreach (var unusedAsset in unusedAssets.OrderBy(x => x.FileSizeKb).Reverse())
+            {
+                logWriter.Log($"UNUSED\t{unusedAsset.FilePath}\t{unusedAsset.FileSizeKb:### ### ##0.0} kb");
+            }
+            var reportName = $"{Path.GetFileNameWithoutExtension(buildReport)}_report.log";
+            logWriter.Save(reportName);
+            Debug.Log($"Report save in {reportName}");
         }
 
-        private static int excludedAssetCount;
+        private static int _excludedAssetCount;
 
-        private static List<AssetLine> checkUnusedAssets(HashSet<string> usedAssets)
+        private static List<AssetLine> CheckUnusedAssets(HashSet<string> usedAssets, LogWriter logWriter)
         {
-            void handleSubFolder(string parent, List<Regex> _excluded, ref List<AssetLine> _result)
+            void HandleSubFolder(string parent, List<Regex> excluded, ref List<AssetLine> result)
             {
-                if (excludedFolders.Contains(parent))
+                if (ExcludedFolders.Contains(parent))
                 {
-                    excludedAssetCount += 1;
+                    _excludedAssetCount += 1;
                     return;
                 }
                 string[] guids = AssetDatabase.FindAssets(null, new[] { parent });
@@ -107,18 +125,18 @@ namespace Editor.Prg.Build
                 {
                     var assetPath = AssetDatabase.GUIDToAssetPath(guid);
                     var isExclude = false;
-                    foreach (var regex in _excluded)
+                    foreach (var regex in excluded)
                     {
                         if (regex.IsMatch(assetPath))
                         {
-                            Debug.Log($"skip {assetPath} ({regex})");
+                            logWriter.Log($"skip {assetPath} ({regex})");
                             isExclude = true;
                             break;
                         }
                     }
                     if (isExclude)
                     {
-                        excludedAssetCount += 1;
+                        _excludedAssetCount += 1;
                     }
                     else
                     {
@@ -130,28 +148,31 @@ namespace Editor.Prg.Build
                                 continue; // Ignore folders
                             }
                             var assetLine = new AssetLine(assetPath, isFile: true);
-                            _result.Add(assetLine);
+                            if (!result.Contains(assetLine))
+                            {
+                                result.Add(assetLine);
+                            }
                         }
                     }
                 }
             }
 
-            var excluded = new List<Regex>();
-            foreach (var excludedFolder in excludedFolders)
+            var excludedList = new List<Regex>();
+            foreach (var excludedFolder in ExcludedFolders)
             {
-                excluded.Add(new Regex(excludedFolder));
+                excludedList.Add(new Regex(excludedFolder));
             }
-            excludedAssetCount = 0;
-            var result = new List<AssetLine>();
+            _excludedAssetCount = 0;
+            var resultList = new List<AssetLine>();
             var folders = AssetDatabase.GetSubFolders("Assets");
             foreach (var folder in folders)
             {
-                handleSubFolder(folder, excluded, ref result);
+                HandleSubFolder(folder, excludedList, ref resultList);
             }
-            return result;
+            return resultList;
         }
 
-        private static List<AssetLine> parseBuildReport(string buildReport, out double totalSize)
+        private static List<AssetLine> ParseBuildReport(string buildReport, out double totalSize)
         {
             const string markerLine = "-------------------------------------------------------------------------------";
             const string assetsLine = "Used Assets and files from the Resources folder, sorted by uncompressed size:";
@@ -167,7 +188,7 @@ namespace Editor.Prg.Build
                         break;
                     }
                     var assetLine = new AssetLine(line);
-                    totalSize += assetLine.fileSizeKb;
+                    totalSize += assetLine.FileSizeKb;
                     result.Add(assetLine);
                 }
                 if (line == assetsLine)
@@ -180,40 +201,86 @@ namespace Editor.Prg.Build
 
         private class AssetLine
         {
-            private static readonly CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
-            private static readonly char[] separators1 = { '%' };
-            private static readonly char[] separators2 = { ' ' };
+            private static readonly CultureInfo Culture = CultureInfo.GetCultureInfo("en-US");
+            private static readonly char[] Separators1 = { '%' };
+            private static readonly char[] Separators2 = { ' ' };
 
-            public readonly double fileSizeKb;
-            public readonly double percentage;
-            public readonly string filePath;
+            private readonly string _line;
+            public readonly double FileSizeKb;
+            public readonly double Percentage;
+            public readonly string FilePath;
 
-            public bool isAsset => filePath.StartsWith("Assets/");
-            public bool isPackage => filePath.StartsWith("Packages/");
-            public bool isResource => filePath.StartsWith("Resources/");
+            public bool IsAsset => FilePath.StartsWith("Assets/");
+            public bool IsPackage => FilePath.StartsWith("Packages/");
+            public bool IsResource => FilePath.StartsWith("Resources/");
+            public bool IsBuiltIn => FilePath.StartsWith("Built-in ");
 
             public AssetLine(string line, bool isFile = false)
             {
+                _line = line ?? string.Empty;
                 if (isFile)
                 {
-                    filePath = line;
-                    fileSizeKb = (int)(new FileInfo(filePath).Length / 1024);
+                    FilePath = _line;
+                    FileSizeKb = (int)(new FileInfo(FilePath).Length / 1024);
                     return;
                 }
-                var tokens = line.Split(separators1);
+                var tokens = _line.Split(Separators1);
                 if (tokens.Length != 2)
                 {
-                    filePath = line;
+                    FilePath = _line;
                     return;
                 }
-                filePath = tokens[1].Trim();
-                tokens = tokens[0].Split(separators2, StringSplitOptions.RemoveEmptyEntries);
+                FilePath = tokens[1].Trim();
+                tokens = tokens[0].Split(Separators2, StringSplitOptions.RemoveEmptyEntries);
                 if (tokens.Length != 3)
                 {
                     return;
                 }
-                fileSizeKb = double.Parse(tokens[0], culture);
-                percentage = double.Parse(tokens[2], culture);
+                FileSizeKb = double.Parse(tokens[0], Culture);
+                Percentage = double.Parse(tokens[2], Culture);
+            }
+
+            private bool Equals(AssetLine other)
+            {
+                return _line == other._line;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj))
+                {
+                    return false;
+                }
+                if (ReferenceEquals(this, obj))
+                {
+                    return true;
+                }
+                if (obj.GetType() != this.GetType())
+                {
+                    return false;
+                }
+                return Equals((AssetLine)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return _line.GetHashCode();
+            }
+        }
+
+        private class LogWriter
+        {
+            private readonly StringBuilder _builder = new StringBuilder();
+
+            public void Log(string line)
+            {
+                Debug.Log(line);
+                _builder.Append(line).AppendLine();
+            }
+
+            public void Save(string fileName)
+            {
+                File.WriteAllText(fileName, _builder.ToString());
             }
         }
     }
