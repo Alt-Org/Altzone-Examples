2ef6ea06054496d4f7a6421c1c7665dd0f5d4ed5
JVDOG <joel.91@hotmail.com>
2022-01-11 11:21:30 +0200

    Avatar and particle Update
    Added shields and particle, updated avatar.

---
+++ Assets/HitParticle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitParticle : MonoBehaviour
+{
+    [SerializeField] ParticleSystem ps;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ball")
+        {
+            Debug.Log("HIT!");
+            foreach (ContactPoint2D hitPos in collision.contacts)
+            {
+                Vector2 hitPoint = hitPos.point;
+                Instantiate(ps, new Vector3(hitPoint.x, hitPoint.y, 0), Quaternion.identity);
+                ps.Play();
+                Debug.Log(hitPos.ToString());
+            }
+        }
+    }
+}
