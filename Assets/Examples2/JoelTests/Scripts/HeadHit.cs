c6169b1595182046a752af43930b3e14cd116c8e
JVDOG <joel.91@hotmail.com>
2022-01-18 08:39:34 +0200

    Shields, particles, side"diamonds", impact sound

---
+++ Assets/Examples2/JoelTests/Scripts/HeadHit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadHit : MonoBehaviour
+{
+    private int _currentCount;
+    public GameObject[] backGround;
+
+    private IEnumerator OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ball")
+        {
+            yield return new WaitForSeconds(.5f);
+            _currentCount++;
+            Debug.Log("Current hit count " + _currentCount.ToString());
+        }
+        if (_currentCount >= 1)
+        {
+            backGround[0].SetActive(false);
+            backGround[1].SetActive(true);
+        }
+        if (_currentCount >= 2)
+        {
+            backGround[1].SetActive(false);
+            backGround[2].SetActive(true);
+        }
+        if (_currentCount >= 3)
+        {
+            backGround[2].SetActive(false);
+            backGround[3].SetActive(true);
+        }
+        if (_currentCount >= 4)
+        {
+            backGround[3].SetActive(false);
+            backGround[4].SetActive(true);
+        }
+    }
+}
--- Assets/Examples2/JoelTests/Scripts/HitParticle.cs
+++ Assets/Examples2/JoelTests/Scripts/HitParticle.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class HitParticle : MonoBehaviour
 {
     [SerializeField] ParticleSystem ps;
+    public AudioSource audioSource;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag(UnityConstants.Tags.Ball))
         {
+            audioSource.PlayOneShot(audioSource.clip);
             Debug.Log("HIT!");
             foreach (ContactPoint2D hitPos in collision.contacts)
             {
                 Vector2 hitPoint = hitPos.point;
                 Instantiate(ps, new Vector3(hitPoint.x, hitPoint.y, 0), Quaternion.identity);
                 ps.Play();
-                Debug.Log(hitPos.ToString());
+                Debug.Log(" x POS " + hitPoint.x.ToString() + " y POS " + hitPoint.y.ToString());
             }
         }
     }
