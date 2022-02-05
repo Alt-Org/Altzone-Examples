ce54b49fbed853836b4d72bd2c5c6d245bd71c4b
Jari Petays <jari.petays@iki.fi>
2022-02-05 19:14:08 +0200

    synchronize player movement over network (tested with 1 player only)

--- Assets/Examples2/Scripts/Battle/Player2/PlayerMovement2.cs
+++ Assets/Examples2/Scripts/Battle/Player2/PlayerMovement2.cs
@@ -1,3 +1,5 @@
+using System;
+using Prg.Scripts.Common.Photon;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,21 +10,30 @@ namespace Examples2.Scripts.Battle.Player2
         private readonly Transform _transform;
         private readonly UnityEngine.InputSystem.PlayerInput _playerInput;
         private readonly Camera _camera;
+        private readonly MovementHelper _helper;
 
         public Rect PlayerArea { get; set; } = Rect.MinMaxRect(-100, -100, 100, 100);
         public float UnReachableDistance { get; set; } = 100;
         public float Speed { get; set; } = 1;
 
         private bool _isMoving;
+        private Vector3 _targetPosition;
+        private Vector3 _tempPosition;
+
         private Vector2 _inputClick;
         private Vector3 _inputPosition;
-        private Vector3 _tempPosition;
 
         public PlayerMovement2(Transform transform, UnityEngine.InputSystem.PlayerInput playerInput, Camera camera)
         {
             _transform = transform;
             _playerInput = playerInput;
             _camera = camera;
+            _helper = new MovementHelper(PhotonEventDispatcher.Get(), (position, speed) =>
+            {
+                _isMoving = true;
+                _targetPosition = position;
+                Speed = speed;
+            });
             SetInput();
         }
 
@@ -30,7 +41,7 @@ namespace Examples2.Scripts.Battle.Player2
         {
             if (_isMoving)
             {
-                MoveTo(_inputPosition, Speed);
+                MoveTo();
             }
         }
 
@@ -39,11 +50,11 @@ namespace Examples2.Scripts.Battle.Player2
             ReleaseInput();
         }
 
-        private void MoveTo(Vector3 position, float speed)
+        private void MoveTo()
         {
-            _tempPosition = Vector3.MoveTowards(_transform.position, position, speed * Time.deltaTime);
+            _tempPosition = Vector3.MoveTowards(_transform.position, _targetPosition, Speed * Time.deltaTime);
             _transform.position = _tempPosition;
-            _isMoving = !(Mathf.Approximately(_tempPosition.x, position.x) && Mathf.Approximately(_tempPosition.y, position.y));
+            _isMoving = !(Mathf.Approximately(_tempPosition.x, _targetPosition.x) && Mathf.Approximately(_tempPosition.y, _targetPosition.y));
         }
 
         private void SetInput()
@@ -79,6 +90,7 @@ namespace Examples2.Scripts.Battle.Player2
             _inputPosition.y += _inputClick.y;
             _inputPosition.x = Mathf.Clamp(_inputPosition.x, PlayerArea.xMin, PlayerArea.xMax);
             _inputPosition.y = Mathf.Clamp(_inputPosition.y, PlayerArea.yMin, PlayerArea.yMax);
+            _helper.SendMsgMoveTo(_inputPosition,Speed);
         }
 
         private void StopMove(InputAction.CallbackContext ctx)
@@ -98,7 +110,39 @@ namespace Examples2.Scripts.Battle.Player2
             _inputPosition = _camera.ScreenToWorldPoint(_inputPosition);
             _inputPosition.x = Mathf.Clamp(_inputPosition.x, PlayerArea.xMin, PlayerArea.xMax);
             _inputPosition.y = Mathf.Clamp(_inputPosition.y, PlayerArea.yMin, PlayerArea.yMax);
-            _inputPosition.z = 0;
+            _helper.SendMsgMoveTo(_inputPosition,Speed);
+        }
+
+        private class MovementHelper
+        {
+            private const int MsgMoveTo = PhotonEventDispatcher.EventCodeBase + 5;
+
+            private readonly PhotonEventDispatcher _photonEventDispatcher;
+            private readonly Action<Vector3, float> _callback;
+
+            private Vector3 _targetPosition;
+
+            public MovementHelper(PhotonEventDispatcher photonEventDispatcher, Action<Vector3, float> onMsgMoveToCallback)
+            {
+                _photonEventDispatcher = photonEventDispatcher;
+                _photonEventDispatcher.RegisterEventListener(MsgMoveTo, data => { OnMsgMoveTo(data.CustomData); });
+                _callback = onMsgMoveToCallback;
+                _targetPosition.z = 0;
+            }
+
+            private void OnMsgMoveTo(object data)
+            {
+                var payload = (float[])data;
+                _targetPosition.x = payload[0];
+                _targetPosition.y = payload[1];
+                _callback.Invoke(_targetPosition, payload[2]);
+            }
+
+            public void SendMsgMoveTo(Vector3 position, float speed)
+            {
+                var payload = new[] { position.x, position.y, speed };
+                _photonEventDispatcher.RaiseEvent(MsgMoveTo, payload);
+            }
         }
     }
 }
