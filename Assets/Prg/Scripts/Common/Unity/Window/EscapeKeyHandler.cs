4b0d75e376e7171d711f25d89abee9a02c0902d9
Jari Petays <jari.petays@iki.fi>
2022-02-18 15:22:14 +0200

    use Keyboard.current.onTextInput callback to listen for Escape key (instead of polling it in Update)

--- Assets/Prg/Scripts/Common/Unity/Window/EscapeKeyHandler.cs
+++ Assets/Prg/Scripts/Common/Unity/Window/EscapeKeyHandler.cs
@@ -1,47 +1,39 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 namespace Prg.Scripts.Common.Unity.Window
 {
     /// <summary>
-    /// Tracks Escape key press so that is must pressed down and released exactly once (on the same level)
-    /// on behalf of <c>WindowManager</c>.
+    /// Tracks Escape key press on behalf of <c>WindowManager</c>
+    /// using <c>UnityEngine.InputSystem</c> with simple callback to listen all keypresses.
     /// </summary>
     public class EscapeKeyHandler : MonoBehaviour
     {
-        private bool _isEscapePressedDown;
-        private bool _isEscapePressedUp;
-        private string _activeScenePathDown;
-        private string _activeScenePathUp;
+        private const char Escape = '\u001B';
 
         private Action _callback;
 
-        private void Update()
+        private void OnEnable()
         {
-            if (Keyboard.current.escapeKey.wasPressedThisFrame)
+            Keyboard.current.onTextInput -= OnTextInput;
+            Keyboard.current.onTextInput += OnTextInput;
+        }
+
+        private void OnDisable()
+        {
+            Keyboard.current.onTextInput -= OnTextInput;
+        }
+
+        private void OnDestroy()
+        {
+            Keyboard.current.onTextInput -= OnTextInput;
+        }
+
+        private void OnTextInput(char c)
+        {
+            if (c == Escape)
             {
-                _activeScenePathDown = SceneManager.GetActiveScene().path;
-                _isEscapePressedDown = true;
-                _isEscapePressedUp = false;
-                return;
-            }
-            if (Keyboard.current.escapeKey.wasReleasedThisFrame)
-            {
-                _activeScenePathUp = SceneManager.GetActiveScene().path;
-                _isEscapePressedUp = true;
-                return;
-            }
-            if (_isEscapePressedDown && _isEscapePressedUp)
-            {
-                _isEscapePressedDown = false;
-                _isEscapePressedUp = false;
-                if (_activeScenePathDown != _activeScenePathUp)
-                {
-                    Debug.LogWarning($"ESCAPE SKIPPED down={_activeScenePathDown} up={_activeScenePathUp}");
-                    return;
-                }
                 _callback?.Invoke();
             }
         }
