ad07fd5c17a5efcf904e14c39c2a365d93bc231c
Miska <miskaharkonen@gmail.com>
2022-03-07 11:33:45 +0200

    FMOD from asset store
    Installed FMOD from Unity asset store and linked it with FMOD Studio.

---
+++ Assets/Prg/Scripts/Service/FMOD/FMODManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Prg.Scripts.Service.FMOD
+{
+    /// <summary>
+    /// AudioManager using FMOD
+    /// </summary>
+    public class FMODManager : MonoBehaviour
+    {
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void BeforeSceneLoad()
+        {
+            var fMODManager = FindObjectOfType<FMODManager>();
+            if (fMODManager == null)
+            {
+                
+                UnityExtensions.CreateGameObjectAndComponent<FMODManager>(nameof(FMODManager), true);
+            }
+        }
+
+    }
+}
