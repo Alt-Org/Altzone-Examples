0032d9667f1c56b9652bb7b25a480c57bbd46da5
Jari Petays <jari.petays@iki.fi>
2022-12-09 13:42:45 +0200

    add SimpleHttpServer

---
+++ Examples/Assets/SimpleHTTPServer/UnityHttpServer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SimpleHTTPServer
+{
+    internal class UnityHttpServer : MonoBehaviour
+    {
+        [SerializeField] private int _port;
+        [SerializeField] private MonoBehaviour _controller;
+
+        private SimpleHttpServer _myServer;
+
+        private void Awake()
+        {
+            _myServer = new SimpleHttpServer(Application.streamingAssetsPath, _port, _controller);
+            _myServer.OnJsonSerialized += JsonUtility.ToJson;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void OnApplicationQuit()
+        {
+            _myServer.Stop();
+        }
+    }
+}
