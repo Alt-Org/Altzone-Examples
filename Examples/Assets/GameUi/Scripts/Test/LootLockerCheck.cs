39ed18f707b3ffe772514ea0918f814b8ea81df6
Jari Petays <jari.petays@iki.fi>
2022-12-19 10:24:49 +0200

    show PlayerName in Editor

--- Examples/Assets/GameUi/Scripts/Test/LootLockerCheck.cs
+++ Examples/Assets/GameUi/Scripts/Test/LootLockerCheck.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using Altzone.Scripts.Service.LootLocker;
+using Prg.Scripts.Common.Unity.Attributes;
 using UnityEngine;
 
 namespace GameUi.Scripts.Test
 {
     public class LootLockerCheck : MonoBehaviour
     {
+        [SerializeField, ReadOnly] private string _playerName;
+        
         private IEnumerator Start()
         {
             Debug.Log($"1 IsRunning {LootLockerWrapper.IsRunning}");
@@ -15,8 +18,8 @@ namespace GameUi.Scripts.Test
             Debug.Log($"3 IsCompleted {result.IsCompleted}");
             yield return new WaitUntil(() => result.IsCompleted);
             Debug.Log($"4 Result {result.Result}");
-            var playerName = LootLockerWrapper.PlayerName;
-            Debug.Log($"5 playerName {playerName}");
+            _playerName = LootLockerWrapper.PlayerName;
+            Debug.Log($"5 playerName {_playerName}");
         }
     }
 }
