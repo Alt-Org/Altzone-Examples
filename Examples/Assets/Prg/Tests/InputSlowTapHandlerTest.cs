61657c0832b12357b4c2f4a2e311d2751aa7672b
Jari Petays <jari.petays@iki.fi>
2023-02-11 08:51:12 +0200

    add ''<Pointer>/position' binding to get pointer position on Tap & SlowTap

--- Examples/Assets/Prg/Tests/InputSlowTapHandlerTest.cs
+++ Examples/Assets/Prg/Tests/InputSlowTapHandlerTest.cs
@@ -19,44 +19,62 @@ namespace Prg.Tests
     public class InputSlowTapHandlerTest : MonoBehaviour, IInputTapHandler
     {
         [SerializeField, Header("Settings")] private InputActionReference _tapActionRef;
+        [SerializeField] private InputActionReference _positionActionRef;
 
         [SerializeField, Header("Debug")] private bool _isLogEvents;
 
-        private InputAction _inputAction;
+        private InputAction _tapAction;
+        private InputAction _positionAction;
+        private Vector2 _startPosition;
         private Vector2 _inputPosition;
         private IInputTapReceiver _tapReceiver;
 
         private void Awake()
         {
-            Debug.Log($"{_tapActionRef}");
-            Assert.IsNotNull(_tapActionRef);
-            _inputAction = _tapActionRef.action;
-            Assert.IsFalse(string.IsNullOrWhiteSpace(_inputAction.interactions));
-            Debug.Log($"interactions {_inputAction.interactions}");
+            Debug.Log($"{_tapActionRef} | {_positionActionRef}");
+            Assert.IsTrue(_tapActionRef != null && _positionActionRef != null);
+            _tapAction = _tapActionRef.action;
+            _positionAction = _positionActionRef.action;
+            Assert.IsFalse(string.IsNullOrWhiteSpace(_tapAction.interactions));
+            Debug.Log($"interactions {_tapAction.interactions}");
         }
 
         private void OnEnable()
         {
-            Debug.Log($"{_inputAction}");
-            _inputAction.performed += TapPerformed;
-            _inputAction.canceled += TapCancelled;
-            _inputAction.Enable();
+            Debug.Log($"{_tapAction} | {_positionAction}");
+            _tapAction.started += TapStarted;
+            _tapAction.performed += TapPerformed;
+            _tapAction.canceled += TapCancelled;
+            _tapAction.Enable();
+            _positionAction.Enable();
         }
 
         private void OnDisable()
         {
-            Debug.Log($"{_inputAction}");
-            _inputAction.performed -= TapPerformed;
-            _inputAction.canceled -= TapCancelled;
-            _inputAction.Disable();
+            Debug.Log($"{_tapAction} | {_positionAction}");
+            _tapAction.started -= TapStarted;
+            _tapAction.performed -= TapPerformed;
+            _tapAction.canceled -= TapCancelled;
+            _tapAction.Disable();
+            _positionAction.Disable();
+        }
+
+        private void TapStarted(InputAction.CallbackContext ctx)
+        {
+            _startPosition = _positionAction.ReadValue<Vector2>();
+            if (_isLogEvents)
+            {
+                Debug.Log($"duration {ctx.duration:0.000} pos {_startPosition} {ctx.interaction?.GetType().Name}");
+            }
         }
 
         private void TapPerformed(InputAction.CallbackContext ctx)
         {
-            _inputPosition = ctx.ReadValue<Vector2>();
+            _inputPosition = _positionAction.ReadValue<Vector2>();
             if (_isLogEvents)
             {
-                Debug.Log($"duration {ctx.duration:0.000} pos {_inputPosition} {ctx.interaction?.GetType().Name}");
+                Debug.Log(
+                    $"duration {ctx.duration:0.000} pos {_inputPosition} delta {_startPosition - _inputPosition} {ctx.interaction?.GetType().Name}");
             }
             if (_tapReceiver == null)
             {
@@ -78,7 +96,8 @@ namespace Prg.Tests
         {
             if (_isLogEvents)
             {
-                Debug.Log($"duration {ctx.duration:0.000} pos {_inputPosition} {ctx.interaction?.GetType().Name}");
+                Debug.Log(
+                    $"duration {ctx.duration:0.000} {ctx.interaction?.GetType().Name}");
             }
         }
 
